c2ad3fa8f96de023ee086902ccddd0021813ce50
Jorge Cano <jorge.cano@activeprospect.com>
2022-05-26 16:17:32 -0500

    Integer.parse solution

--- Day_1/lesson_1.ex
+++ Day_1/lesson_1.ex
@@ -3,7 +3,12 @@ defmodule Scanner do
     # data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
     # data |> Enum.chunk_every(2, 1, :discard) |> Enum.filter(fn [x, y] -> y > x end) |> Enum.count()
     "inputs.txt"
-    |> File.read!() |> String.split("\n") |> Enum.chunk_every(2, 1, :discard) |> Enum.filter(fn [x, y] -> y > x end) |> Enum.count()
+    |> File.read!()
+    |> String.split("\n")
+    |> Enum.map(&Integer.parse/1)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.filter(fn [x, y] -> y > x end)
+    |> Enum.count()
   end
 end
-IO.inspect(Scanner.depth_increases)
+IO.inspect(Scanner.depth_increases())
