99cb7d63d0abd2e0805651f5b6f1eb0426e2c63c
Jorge Cano <jorge.cano@activeprospect.com>
2022-05-26 15:17:40 -0500

    Working output, but incorrect ans

---
+++ Day_1/lesson_1.ex
@@ -0,0 +1,9 @@
+defmodule Scanner do
+  def depth_increases do
+    # data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    # data |> Enum.chunk_every(2, 1, :discard) |> Enum.filter(fn [x, y] -> y > x end) |> Enum.count()
+    "inputs.txt"
+    |> File.read!() |> String.split("\n") |> Enum.chunk_every(2, 1, :discard) |> Enum.filter(fn [x, y] -> y > x end) |> Enum.count()
+  end
+end
+IO.inspect(Scanner.depth_increases)
--- lesson_1.ex
+++
@@ -1,7 +0,0 @@
-defmodule Scanner do
-  def depth_increases do
-    data = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-    data |> Enum.chunk_every(2, 1, :discard) |> Enum.filter(fn [x, y] -> y > x end) |> Enum.count()
-  end
-end
-IO.inspect(Scanner.depth_increases)
